ca41b51249c4c856442a7641aedfdca1ac24cf11
Marino Botina <76748860+fili258-oss@users.noreply.github.com>
2024-05-28 18:49:04 -0500

    Update TravelsApplicationTests.java

--- src/test/java/com/tour/travels/TravelsApplicationTests.java
+++ src/test/java/com/tour/travels/TravelsApplicationTests.java
@@ -2,12 +2,13 @@ package com.tour.travels;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TravelsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
